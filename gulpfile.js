5a044e6706bc38e8176502b8f08db7d7e85bac7a
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2014-02-09 11:39:28 +0200

    Added the connect task

--- gulpfile.js
+++ gulpfile.js
@@ -5,84 +5,52 @@
  */
 
 // dependencies
-var gulp     = require('gulp'),
-    express  = require('express'),
-    sass     = require('gulp-ruby-sass'),
-    prefixer = require('gulp-autoprefixer'),
-    filter   = require('gulp-filter'),
-    lr       = require('tiny-lr')();
+var gulp     = require('gulp');
 
+// Load plugins
+var $ = require('gulp-load-plugins')();
 
 // settings
 var assets = './assets',
     sass_dir = assets + '/scss',
     css_dir = assets + '/css',
-    js_dir = assets + '/js',
-    EXPRESS_PORT = 8080,
-    LIVERELOAD_PORT = 35729,
-    EXPRESS_ROOT = __dirname;
-
+    js_dir = assets + '/js';
 
 // compile sass
 gulp.task('sass', function () {
 return gulp.src(sass_dir + '/**/*.{scss,sass}')
-           .pipe(sass({ style : 'expanded' })).on('error', function(e){console.log('\x07',e.message);})
-           .pipe(filter('**/*.css'))
-           .pipe(prefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-           .pipe(gulp.dest(css_dir));
+           .pipe($.rubySass({ style : 'expanded' })).on('error', function(e){console.log('\x07',e.message);})
+           .pipe($.filter('**/*.css'))
+           .pipe($.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+           .pipe(gulp.dest(css_dir))
+           .pipe($.connect.reload());
 });
 
 
-
-
-
-
-
+// Connect
+gulp.task('connect', $.connect.server({
+    root: __dirname,
+    port: 9000,
+    livereload: true,
+    open: {
+      browser: 'chrome' // if not working OS X browser: 'Google Chrome'
+    }
+}));
+gulp.task('reload', function () {
+  return gulp.src([js_dir + '/**/*.js', 'index.html'])
+             .pipe($.connect.reload());
+});
 
 // the development task
-gulp.task('default', ['sass'], function() {
-    // start up the server
-    startExpress(EXPRESS_PORT, EXPRESS_ROOT);
-    startLiveReload(LIVERELOAD_PORT);
+gulp.task('default', ['sass', 'connect'], function() {
 
     // watch for sass files
     gulp.watch(sass_dir + '/*.{scss,sass}', ['sass']);
 
     // livereload the necesary files
-    gulp.watch(css_dir + '/**/*.css', notifyLivereload);
-    gulp.watch(js_dir + '/**/*.js', notifyLivereload);
-    gulp.watch('index.html', notifyLivereload);
+    gulp.watch([js_dir + '/**/*.js', 'index.html'], ['reload']);
 });
 
 
 // the build task
 gulp.task('build', ['sass']);
-
-// helper functions
-function startExpress(port, root) {
-    var app = express();
-
-    port = port || 8080;
-    root = root || __dirname;
-
-    app.use(require('connect-livereload')());
-    app.use(express.static(root));
-    app.listen(port);
-}
-
-function startLiveReload(port) {
-    port = port || 35729;
-    lr.listen(port);
-}
-
-// Notifies livereload of changes detected
-// by `gulp.watch()`
-function notifyLivereload(event) {
-  var fileName = require('path').relative(EXPRESS_ROOT, event.path);
-
-  lr.changed({
-    body: {
-      files: [fileName]
-    }
-  });
-}
